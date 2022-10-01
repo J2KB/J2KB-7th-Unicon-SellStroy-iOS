73c13b510fa1f55cb1cadf481651d9287e2fa3ea
min <75626237+mingging@users.noreply.github.com>
2022-10-02 00:01:27 +0900

    [ADD] 프로젝트 세팅

---
+++ SellStroy-iOS/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SellStroy-iOS
+//
+//  Created by minimani on 2022/10/01.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow()
+        let mainViewController = MainViewController()
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ SellStroy-iOS/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SellStroy-iOS
+//
+//  Created by minimani on 2022/10/01.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
