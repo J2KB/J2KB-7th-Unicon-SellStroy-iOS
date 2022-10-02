4497ce2d4f2d0397cdd7c78f0e5d8a5a2768f373
min <75626237+mingging@users.noreply.github.com>
2022-10-03 00:41:44 +0900

    [ADD] 홈 화면 작업

---
+++ SellStroy-iOS/Common/Colors.swift
@@ -0,0 +1,35 @@
+//
+//  Colors.swift
+//  SellStroy-iOS
+//
+//  Created by minimani on 2022/10/03.
+//
+
+import UIKit
+
+extension UIColor {
+    class var SSRed: UIColor {
+        // rgba(255, 68, 68, 1)
+        return UIColor(red: 255.0 / 255.0, green: 68.0 / 255.0, blue: 68.0 / 255.0, alpha: 1)
+    }
+    
+    class var SSGray1: UIColor {
+        // rgba(236, 236, 236, 1)
+        return UIColor(red: 236.0 / 255.0, green: 236.0 / 255.0, blue: 236.0 / 255.0, alpha: 1)
+    }
+    
+    class var SSGray2: UIColor {
+        // rgba(217, 217, 217, 1)
+        return UIColor(red: 217.0 / 255.0, green: 217.0 / 255.0, blue: 217.0 / 255.0, alpha: 1)
+    }
+    
+    class var SSGray3: UIColor {
+        // rgba(175, 175, 175, 1)
+        return UIColor(red: 175.0 / 255.0, green: 175.0 / 255.0, blue: 175.0 / 255.0, alpha: 1)
+    }
+    
+    class var SSGray4: UIColor {
+        // rgba(104, 104, 104, 1)
+        return UIColor(red: 104.0 / 255.0, green: 104.0 / 255.0, blue: 104.0 / 255.0, alpha: 1)
+    }
+}
